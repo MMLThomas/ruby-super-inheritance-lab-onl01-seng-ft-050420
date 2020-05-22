a90984aa2e8a41891fcf8f77b83c66bef40378f2
michael thomas <michael.thomas2014@gmail.com>
2020-05-22 09:34:29 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -12,12 +12,13 @@ class ChattyStudent < Student
   end
   
   def raise_hand
-  
-    i = 0
+  super
+    i = 1
     while i < 10 
-    super
+    puts "Pick me!"
       i += 1
     end
+
   end
   
   
